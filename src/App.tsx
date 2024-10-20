eefba875eacdfc5de6a9b18ca5b50603ac6b2eae
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-20 23:53:36 +0200

    Buttons added from antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import React from 'react';
+import { Button, Flex } from 'antd';
+
 const App = () => {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <Flex gap="small" wrap>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+      </Flex>
+    </div>
   )
 }
 
