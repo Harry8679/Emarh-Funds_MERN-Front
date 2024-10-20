9747f60ec9401bc5a78f3d9e5a01304d06c966ab
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-21 00:12:55 +0200

    ThemeProvider added in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Button, Flex } from 'antd';
+import ThemeProvider from './providers/theme-provider';
 
 const App : React.FC = () => {
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <Flex gap="small" wrap>
-        <Button type="primary">Primary Button</Button>
-        <Button>Default Button</Button>
-        <Button type="dashed">Dashed Button</Button>
-        <Button type="text">Text Button</Button>
-        <Button type="link">Link Button</Button>
-      </Flex>
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <Flex gap="small" wrap>
+          <Button type="primary">Primary Button</Button>
+          <Button>Default Button</Button>
+          <Button type="dashed">Dashed Button</Button>
+          <Button type="text">Text Button</Button>
+          <Button type="link">Link Button</Button>
+        </Flex>
+      </div>
+    </ThemeProvider>
   )
 }
 
