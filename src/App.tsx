6779d624e8004df29e6974a8384e44bcbd681007
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-22 18:17:16 +0200

    Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Button, Flex } from 'antd';
 import ThemeProvider from './providers/theme-provider';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/auth/login/Login';
+import RegisterPage from './pages/auth/register/Register';
+import HomePage from './pages/private/home/Home';
 
 const App : React.FC = () => {
 
   return (
     <ThemeProvider>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <Flex gap="small" wrap>
-          <Button type="primary">Primary Button</Button>
-          <Button>Default Button</Button>
-          <Button type="dashed">Dashed Button</Button>
-          <Button type="text">Text Button</Button>
-          <Button type="link">Link Button</Button>
-        </Flex>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
