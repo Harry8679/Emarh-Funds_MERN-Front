a77ae90c27fc521cd92b91476a67dceb92637263
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-22 19:18:06 +0200

    Register Page Welcome Content

--- src/pages/auth/common/WelcomeContent.tsx
+++ src/pages/auth/common/WelcomeContent.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const WelcomeContent : React.FC = () => {
     return (
-        <div>Welcome Content</div>
+        <div className='flex flex-col items-center gap-2'>
+            <img src="https://static.vecteezy.com/system/resources/previews/010/880/186/original/charity-hand-sign-inside-a-love-shape-international-day-of-charity-png.png" alt="charity" 
+                className='w-72 h-60'
+            />
+            <h1 className="text-5xl text-[#FF5555] font-bold">EMARH - FUNDS</h1>
+            <span className="text-sm text-gray-200">
+                Happiness doesn't result from what we get, but from what we give.
+            </span>
+        </div>
     );
 };
 
